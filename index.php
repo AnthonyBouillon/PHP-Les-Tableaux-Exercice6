9c72bc1f53428a5dd9d0015932e5e2e4c63b6852
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 13:59:35 +0100

    init commit from bash for PHP-Les-Tableaux-Exercice6

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les tableaux Exercice 6</title>
+  </head>
+  <body>
+    <?php
+    //  => = associe un index à une valeur exemple : Nom => Cremefraiche
+      $department = array('02' => 'Aisne', '59' => 'Nord', '60' => 'Oise', '62' => 'Pas-de-Calais', '80' => 'Somme');
+      // Affiche la valeur de l'index 59
+      echo $department['59'];
+     ?>
+  </body>
+</html>
